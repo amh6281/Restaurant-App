d830fdbe65c55538036a50cec6d47d3c07bbd52b
amh6281 <amh6281@naver.com>
2023-07-20 12:58:38 +0900

    Navbar logo design 2023.07.20

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+      {/* Logo */}
+      <div className="text-xl">
+        <Link href="/">roma</Link>
+      </div>
+      {/* Mobile Menu */}
+      <Menu />
+    </div>
+  );
 };
 
 export default Navbar;
