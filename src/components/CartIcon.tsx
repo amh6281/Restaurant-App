5bdc577a0dd01e9d54daeac2f86766d09de0ae65
amh6281 <amh6281@naver.com>
2023-07-21 10:36:26 +0900

    Navbar 반응형 구현 2023.07.21

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const CartIcon = () => {
   return (
-    <div className="flex items-center gap-4">
-      <div className="relative w-8 h-8">
+    <div className="flex items-center gap-1">
+      <div className="relative w-8 h-8 md:w-5 md:h-5">
         <Image src="/cart.png" alt="" fill />
       </div>
       <span>장바구니 (3)</span>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
+
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-16 lg:px-20 xl:px-40">
+      {/* Left Links */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">홈</Link>
+        <Link href="menu">메뉴</Link>
+        <Link href="/">문의하기</Link>
+      </div>
       {/* Logo */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">roma</Link>
       </div>
       {/* Mobile Menu */}
-      <Menu />
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* Right Links */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>031-123-1234</span>
+        </div>
+        {!user ? (
+          <Link href="/login">로그인</Link>
+        ) : (
+          <Link href="/orders">주문내역</Link>
+        )}
+        <CartIcon />
+      </div>
     </div>
   );
 };
