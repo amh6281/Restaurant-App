e26e041c0430fe7d30fe975e30631c8f0b566e40
amh6281 <amh6281@naver.com>
2023-07-22 11:15:50 +0900

    FeaturedProducts data 추가 2023.07.22

---
+++ src/data.ts
@@ -0,0 +1,202 @@
+type Product = {
+  id: number;
+  title: string;
+  desc?: string;
+  img?: string;
+  price: number;
+  options?: { title: string; additionalPrice: number }[];
+};
+
+type Products = Product[];
+
+export const featuredProducts: Products = [
+  {
+    id: 1,
+    title: "콤비네이션",
+    desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
+    img: "/temporary/p1.png",
+    price: 19000,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6000,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "베이컨 디럭스",
+    desc: "Indulge in smoky goodness with a flame-grilled beef patty, topped with crispy bacon, melted cheddar cheese, caramelized onions, and a smattering of tangy BBQ sauce.",
+    img: "/temporary/p2.png",
+    price: 29900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6000,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "나폴리 피자",
+    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
+    img: "/temporary/p3.png",
+    price: 24900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6000,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "매콤 아라비아따",
+    desc: "Ignite your taste buds with this fiery pasta creation, combining penne in a spicy tomato sauce infused with garlic, red chili flakes, and fresh basil for the ultimate comfort food experience.",
+    img: "/temporary/p4.png",
+    price: 26900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6000,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "치즈 지옥",
+    desc: "Ignite your taste buds with a fiery kick! This burger features a succulent beef patty, fiery jalapeños, pepper jack cheese, and a zesty chipotle mayo sauce , and all the classic fixings on a toasted bun.",
+    img: "/temporary/p5.png",
+    price: 29900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 1500,
+      },
+      {
+        title: "Large",
+        additionalPrice: 3000,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "마르게리타",
+    desc: "A timeless favorite with a twist, showcasing a thin crust topped with sweet tomatoes, fresh basil, creamy mozzarella, and a drizzle of extra virgin olive oil, fresh arugula, and a drizzle of balsamic glaze.",
+    img: "/temporary/p6.png",
+    price: 24900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6000,
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "갈릭 파마산 푸실리",
+    desc: "A garlic lover's delight, featuring linguine smothered in a creamy Parmesan sauce, infused with garlic and garnished with chopped parsley, bell peppers, and cherry tomatoes.",
+    img: "/temporary/p7.png",
+    price: 28900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 3000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 5000,
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "치즈 피자",
+    desc: "Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.",
+    img: "/temporary/p8.png",
+    price: 12900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 2000,
+      },
+      {
+        title: "Large",
+        additionalPrice: 4000,
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "하와이안 데리야끼",
+    desc: "Experience a taste of the tropics with a juicy beef patty glazed in tangy teriyaki sauce, topped with grilled pineapple, crispy bacon, and fresh lettuce, and all the classic fixings on a toasted bun.",
+    img: "/temporary/p9.png",
+    price: 21900,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 1500,
+      },
+      {
+        title: "Large",
+        additionalPrice: 3000,
+      },
+    ],
+  },
+];
