9587287a1b8c915d5aebfb6c0cd59f63e3d81022
amh6281 <amh6281@naver.com>
2023-08-11 13:38:58 +0900

    Fetch All Categories api 구현 2023.08.11
    PrismaClient를 이용해 JS로 prisma의 DB 작업 관리

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,9 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return new NextResponse("Hello", { status: 200 });
-};
+// JS를 이용해 prisma의 DB 작업 관리를 위한 PrismaClient
+const prisma = new PrismaClient();
 
-export const POST = () => {
-  return new NextResponse("Hello", { status: 200 });
+// Fetch All Categories
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(JSON.stringify({ message: "잘못된 접근입니다." }), {
+      status: 500,
+    });
+  }
 };
