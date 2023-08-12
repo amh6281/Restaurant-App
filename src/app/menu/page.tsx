8d6c88ec0e7a94e8a3a646a767bb4d2e34ff7143
amh6281 <amh6281@naver.com>
2023-08-12 09:42:10 +0900

    Menu page data fetch 2023.08.12
    Menu page categories data fetch

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,20 @@
-import { menu } from "@/data";
+import { MenuType } from "@/types/types";
 import Link from "next/link";
 import React from "react";
 
-const MenuPage = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("실패");
+  }
+  return res.json();
+};
+
+const MenuPage = async () => {
+  const menu: MenuType = await getData();
+
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
@@ -18,7 +30,7 @@ const MenuPage = () => {
             <button
               className={`hidden 2xl:block bg-${category.color} text-${
                 category.color === "black" ? "white" : "red-500"
-              } py-2 px-4 rounded-md font-bold`}
+              } py-2 px-4 rounded-md`}
             >
               더보기
             </button>
