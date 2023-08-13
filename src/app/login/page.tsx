d2910a3e9dc2a41681c0140aac3d187891462805
amh6281 <amh6281@naver.com>
2023-08-13 10:36:00 +0900

    Login page 로그인 기능 구현 2023.08.13
    useSession hook을 사용하여 로그인 버튼 클릭시 signIn 호출과 함께 status, data를 가져온다.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { useSession, signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const { data, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* Box */}
@@ -13,7 +27,10 @@ const LoginPage = () => {
         </div>
         {/* Form Container */}
         <div className="p-10 flex flex-col gap-8 md:w-1/2 justify-center">
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+          <button
+            className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md"
+            onClick={() => signIn("google")}
+          >
             <Image
               src="/google.png"
               alt=""
