efde4cde6c2af56d7e9f629a815d9a996275efc8
amh6281 <amh6281@naver.com>
2023-08-18 10:20:11 +0900

    CartIcon component store state fetch 2023.08.18

--- src/components/navbar/CartIcon.tsx
+++ src/components/navbar/CartIcon.tsx
@@ -1,13 +1,18 @@
+"use client";
+
+import { useCartStore } from "@/utils/store";
 import Image from "next/image";
 import React from "react";
 
 const CartIcon = () => {
+  const { totalItems } = useCartStore();
+
   return (
     <div className="flex items-center gap-1">
       <div className="relative w-8 h-8 md:w-5 md:h-5">
         <Image src="/cart.png" alt="" fill />
       </div>
-      <span>장바구니 (3)</span>
+      <span>장바구니 ({totalItems})</span>
     </div>
   );
 };
