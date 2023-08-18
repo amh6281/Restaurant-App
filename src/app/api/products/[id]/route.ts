8aafe78591910c9941f01b08d32efe5d74cac252
amh6281 <amh6281@naver.com>
2023-08-18 09:15:35 +0900

    Get product api 생성 2023.08.18

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,27 +1,22 @@
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async (
+// Get Single Product
+export const GET = async (
   req: NextRequest,
   { params }: { params: { id: string } }
 ) => {
   const { id } = params;
 
   try {
-    const body = await req.json();
-
-    await prisma.order.update({
+    const product = await prisma.product.findUnique({
       where: {
         id: id,
       },
-      data: { status: body },
     });
-    return new NextResponse(
-      JSON.stringify({ message: "주문이 업데이트되었습니다.." }),
-      {
-        status: 200,
-      }
-    );
+    return new NextResponse(JSON.stringify(product), {
+      status: 200,
+    });
   } catch (err) {
     console.log(err);
     return new NextResponse(JSON.stringify({ message: "잘못된 접근입니다." }), {
