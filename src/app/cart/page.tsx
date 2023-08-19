e01ed2b2c8c38f5ddb20f0d3903a8fe9d21fd65b
amh6281 <amh6281@naver.com>
2023-08-19 11:14:24 +0900

    Product 중복 제거 2023.08.19

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -23,7 +23,9 @@ const CartPage = () => {
               <Image src={item.img} alt="" width={100} height={100} />
             )}
             <div>
-              <h1 className="text-xl font-bold">{item.title}</h1>
+              <h1 className="text-xl font-bold">
+                {item.title} x {item.quantity}
+              </h1>
               <span>{item.optionTitle}</span>
             </div>
             <h2 className="font-bold">{item.price}₩</h2>
