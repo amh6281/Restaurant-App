ba9cb2b491fd97c788a8823a9832108131f205ca
amh6281 <amh6281@naver.com>
2023-08-20 09:37:30 +0900

    Product delete api 생성 2023.08.20

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -23,4 +24,35 @@ export const GET = async (
       status: 500,
     });
   }
+};
+
+// Delete Single Product
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const session = await getAuthSession();
+
+  if (session?.user.isAdmin) {
+    try {
+      await prisma.product.delete({
+        where: {
+          id: id,
+        },
+      });
+      return new NextResponse(JSON.stringify("삭제하였습니다."), {
+        status: 200,
+      });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "잘못된 접근입니다." }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(JSON.stringify({ message: "허용되지 않았습니다." }), {
+    status: 403,
+  });
 };
