21118cd0f659523ed0f82b9bafdaa1d8d8009cb7
amh6281 <amh6281@naver.com>
2023-08-21 09:32:59 +0900

    Create order api 생성 2023.08.21

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -32,3 +32,29 @@ export const GET = async (req: NextRequest) => {
     );
   }
 };
+
+// Create Order
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+      const order = await prisma.order.create({
+        data: body,
+      });
+      return new NextResponse(JSON.stringify(order), { status: 201 });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "잘못된 접근입니다." }),
+        { status: 500 }
+      );
+    }
+  } else {
+    return new NextResponse(
+      JSON.stringify({ message: "인증되지 않았습니다." }),
+      { status: 401 }
+    );
+  }
+};
